2ecc8a41eb2091c1d0eaea3a20853c5469fc826c
Xiaojun Bao <baoxiaojun43@gmail.com>
2020-02-08 15:16:24 -0600

    dt

--- SL/assignment1/dt.py
+++ SL/assignment1/dt.py
@@ -5,7 +5,7 @@ Created on Thu Feb  6 22:59:12 2020
 @author: Xiaojun
 """
 
-from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
