7bc1b8bce78eae14fa015c0f26b8182a559d6826
Xiaojun Bao <baoxiaojun43@gmail.com>
2020-03-01 16:06:29 -0600

    add nn metrics

--- RO/assignment2/nn.py
+++ RO/assignment2/nn.py
@@ -2,11 +2,10 @@ import os
 import mlrose_hiive as mlrose
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 from time import process_time
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 
 # load data
 dat = pd.read_csv(os.path.join("data", "dataset_31_credit-g.csv"))
@@ -31,6 +30,10 @@ algo_list = [
 curve_list = []
 acc_train_list = []
 acc_test_list = []
+prec_train_list = []
+prec_test_list = []
+recall_train_list = []
+recall_test_list = []
 f1_train_list = []
 f1_test_list = []
 time_list = []
@@ -58,6 +61,10 @@ for algo in algo_list:
     y_pred_test = nn.predict(X_test)
     acc_train = accuracy_score(y_train, y_pred_train)
     acc_test = accuracy_score(y_test, y_pred_test)
+    prec_train = precision_score(y_train, y_pred_train)
+    prec_test = precision_score(y_test, y_pred_test)
+    recall_train = recall_score(y_train, y_pred_train)
+    recall_test = recall_score(y_test, y_pred_test)
     f1_train = f1_score(y_train, y_pred_train)
     f1_test = f1_score(y_test, y_pred_test)
 
@@ -66,6 +73,10 @@ for algo in algo_list:
     curve_list.append(curve)
     acc_train_list.append(acc_train)
     acc_test_list.append(acc_test)
+    prec_train_list.append(prec_train)
+    prec_test_list.append(prec_test)
+    recall_train_list.append(recall_train)
+    recall_test_list.append(recall_test)
     f1_train_list.append(f1_train)
     f1_test_list.append(f1_test)
 
@@ -76,7 +87,7 @@ acc_df = pd.DataFrame(
 print(acc_df)
 acc_df.plot(style=".-")
 plt.ylabel("accuracy")
-plt.title("Neural Network Weight Optimization: model performance vs algorithms")
+plt.title("Neural Network Weight Optimization: accuracy vs algorithms")
 plt.savefig("output/nn_acc.png")
 plt.close()
 
@@ -86,8 +97,29 @@ f1_df = pd.DataFrame(
 print(f1_df)
 f1_df.plot(style=".-")
 plt.ylabel("f1")
-plt.title("Neural Network Weight Optimization: model performance vs algorithms")
+plt.title("Neural Network Weight Optimization: f1-score vs algorithms")
 plt.savefig("output/nn_f1.png")
+plt.close()
+
+prec_df = pd.DataFrame(
+    data={"prec_train": prec_train_list, "prec_test": prec_test_list}, index=algo_list,
+)
+print(prec_df)
+prec_df.plot(style=".-")
+plt.ylabel("precision")
+plt.title("Neural Network Weight Optimization: precision vs algorithms")
+plt.savefig("output/nn_prec.png")
+plt.close()
+
+recall_df = pd.DataFrame(
+    data={"recall_train": recall_train_list, "recall_test": recall_test_list},
+    index=algo_list,
+)
+print(recall_df)
+recall_df.plot(style=".-")
+plt.ylabel("precision")
+plt.title("Neural Network Weight Optimization: recall vs algorithms")
+plt.savefig("output/nn_recall.png")
 plt.close()
 
 df = pd.DataFrame(curve_list).transpose()
